e67bcb4ee6d314f4663e288c0705b6a29b7fc4b9
Sashkakotov <sashkakotov@mail.ru>
2023-01-04 22:11:32 +0300

    fix: src logo

--- online-store/src/views/pages/cart/cartPage.ts
+++ online-store/src/views/pages/cart/cartPage.ts
@@ -1,3 +1,4 @@
+import { CardLogo } from './../../components/constants';
 import { ProductCartChilds, counterChange } from "../../components/constants";
 import { setCartTotal } from "../../components/state";
 import productItems from "../../components/productJSON";
@@ -96,7 +97,7 @@ export function getCartHtml() {
               <div class="card-data">
                 <div class="card-data-number">
                   <img class="card-img"
-                    src="https://i.guim.co.uk/img/media/b73cc57cb1d46ae742efd06b6c58805e8600d482/16_0_2443_1466/master/2443.jpg?width=700&quality=85&auto=format&fit=max&s=fb1dca6cdd4589cd9ef2fc941935de71"
+                    src=${CardLogo.noLogo}
                     alt=""
                   />
                   <input
